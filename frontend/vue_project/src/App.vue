dc67d294fb2df3c08f5668e3317fb212edbe2f0a
Radoman Gledovic <developer.1996@outlook.com>
2020-11-14 17:16:48 +0100

    Update frontend

--- frontend/vue_project/src/App.vue
+++ frontend/vue_project/src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    
+  <div class="container-fluid">
+    <app-header></app-header>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import Header from './components/Header/Header.vue';
   export default {
-    
+      components: {
+          appHeader: Header
+      }
   }
 </script>
 
 <style scoped>
-
+  .container-fluid {
+    padding-left: 0;
+    padding-right: 0;
+  }
 </style>
--- frontend/vue_project/src/main.js
+++ frontend/vue_project/src/main.js
@@ -1,7 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import Vuelidate from 'vuelidate';
+
+import { routes } from './routes';
+import { store } from './store/store';
+
+Vue.use(Vuelidate);
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App)
 })
