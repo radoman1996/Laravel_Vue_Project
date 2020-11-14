db0d976465df75fedbacd9492e6a3325e970d89d
Radoman Gledovic <developer.1996@outlook.com>
2020-11-14 17:14:32 +0100

    Add AllProducts.vue

---
+++ frontend/vue_project/src/components/Products/All_Products/AllProducts.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+        <app-product v-for="product in products" :product="product"></app-product>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    import * as types from '../../../store/types';
+    import Product from './Product/Product.vue';
+
+    export default {
+        computed: {
+            products() {
+                return this.$store.getters[types.PRODUCTS];
+            }
+        },
+        components: {
+            appProduct: Product
+        }
+    }
+</script>
+
+<style scoped>
+    
+</style>
