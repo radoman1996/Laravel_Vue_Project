72be8fbec876a9dc1e27f84ba59fe0bc55a184a2
Radoman Gledovic <developer.1996@outlook.com>
2020-11-14 17:18:08 +0100

    Create custom migrations

---
+++ backend/laravel_project/database/migrations/2020_11_11_234035_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->float('price');
+            $table->mediumText('description');
+            $table->string('image');
+            $table->integer('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->integer('brand_id');
+            $table->foreign('brand_id')->references('id')->on('brands');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
