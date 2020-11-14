83a75b20a47294de36b0d8a80ed6b7b508aacd9d
Radoman Gledovic <developer.1996@outlook.com>
2020-11-14 17:15:34 +0100

    Add types.js

---
+++ frontend/vue_project/src/store/types.js
@@ -0,0 +1,30 @@
+//getters
+export const USER = 'userdata/USER';
+export const IS_USER_LOGGED_IN = 'checkuserloggedin/IS_USER_LOGGED_IN';
+export const PRODUCT = 'product/PRODUCT';
+export const EDIT_PRODUCT = 'product/EDIT_PRODUCT';
+export const PRODUCTS = 'products/PRODUCTS';
+export const BRANDS = 'brands/BRANDS';
+export const CATEGORIES = 'categories/CATEGORIES';
+
+//mutations
+export const MUTATE_CHANGE_USER = 'userdata/MUTATE_CHANGE_USER';
+export const MUTATE_CHANGE_LOGGED_IN_STATUS = 'checkuserloggedin/MUTATE_CHANGE_LOGGED_IN_STATUS';
+export const MUTATE_CHANGE_PRODUCT = 'product/MUTATE_CHANGE_PRODUCT';
+export const MUTATE_CHANGE_EDIT_PRODUCT = 'product/MUTATE_CHANGE_EDIT_PRODUCT';
+export const MUTATE_FILL_PRODUCTS_ARRAY = 'products/MUTATE_FILL_PRODUCTS_ARRAY';
+export const MUTATE_DELETE_ELEMENT_FROM_PRODUCTS = 'products/MUTATE_DELETE_ELEMENT_FROM_PRODUCTS';
+export const MUTATE_FILTER_PRODUCTS = 'products/MUTATE_FILTER_PRODUCTS';
+export const MUTATE_FILL_BRANDS = 'brands/MUTATE_FILL_BRANDS';
+export const MUTATE_FILL_CATEGORIES = 'categories/MUTATE_FILL_CATEGORIES';
+
+//actions
+export const CHANGE_USER = 'userdata/CHANGE_USER';
+export const CHANGE_LOGGED_IN_STATUS = 'checkuserloggedin/CHANGE_LOGGED_IN_STATUS';
+export const CHANGE_PRODUCT = 'product/CHANGE_PRODUCT';
+export const CHANGE_EDIT_PRODUCT = 'product/CHANGE_EDIT_PRODUCT';
+export const FILL_PRODUCTS_ARRAY = 'products/FILL_PRODUCTS_ARRAY';
+export const DELETE_ELEMENT_FROM_PRODUCTS = 'products/DELETE_ELEMENT_FROM_PRODUCTS';
+export const FILTER_PRODUCTS = 'products/FILTER_PRODUCTS';
+export const FILL_BRANDS = 'brands/FILL_BRANDS';
+export const FILL_CATEGORIES = 'categories/FILL_CATEGORIES';
